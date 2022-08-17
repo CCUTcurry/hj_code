baccc4686bf79115885c93389650e9da21933a85
shrlingithub <shrlinchen0702@gmail.com>
2022-08-17 09:56:11 +0800

    11

--- functions/MainProcess.py
+++ functions/MainProcess.py
@@ -11,6 +11,19 @@ from functions.DataCleaning import *
 import pandas as pd
 
 
+class LR:
+    def __init__(self):
+        pass
+    
+
+class Kmeans:
+    def __init__(self):
+        pass
+
+class Xgboost(self):
+    def __init__(self):
+        pass
+
 if __name__ == '__main__':
     data = pd.read_csv('mfd_bank_shibor.csv')
 
